199520b037c72290fbcddbe0e73cccc34946f687
evelyn <edtwinfish@gmail.com>
2018-08-23 20:26:52 -0700

    linq orderby

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -8,48 +8,98 @@ namespace Practice
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            //sort array
-            // Create a delicious data source.
-            string[] fruits = { "coconut", "apple", "blueberry" };
+            Console.WriteLine("\n\nMENU \n-----------------\n");
+            Console.WriteLine("1 sortDogsArray\n");
+            Console.WriteLine("2 sortAgeArray\n");
+            Console.WriteLine("0 Exit");
+            Console.WriteLine("Please select the menu number to run!\n");
+            var menu = Console.ReadLine();
 
-            // Query for ascending sort.
-            IEnumerable<string> sortAscendingQuery =
-                from fruit in fruits
-                orderby fruit //"ascending" is default
-            select fruit;
-
-            // Query for descending sort.
-            IEnumerable<string> sortDescendingQuery =
-                from w in fruits
-                orderby w descending
-                select w;
-
-            // Execute the query.
-            Console.WriteLine("Ascending:");
-            foreach (string s in sortAscendingQuery)
+            switch(menu)
             {
-                Console.WriteLine(s);
-            }
-
-            // Execute the query.
-            Console.WriteLine(Environment.NewLine + "Descending:");
-            foreach (string s in sortDescendingQuery)
-            {
-                Console.WriteLine(s);
-            }
-
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
-
+                case "1":
+                    Console.WriteLine("sortDogsArray");
+                    sortDogsArray();
+                    break;
+                case "2":
+                    Console.WriteLine("sortAgeArray");
+                    sortAgeArray();
+                    break;
+                case "0":                    
+                    break;
+            }; 
 
             //find item in array
             //sort multi-dimensional array
             //find item in multi-dimensional array
             //sort list
             //find item in list
+        }
+
+        public static void sortDogsArray()
+        {
+            //sort array
+            string[] dogs = { "abe", "charlie", "springer", "dodger" };
+            Console.WriteLine("dogs: ");
+            foreach (string dog in dogs)
+            {
+                Console.WriteLine(dog);
+            }
+
+            IEnumerable<string> dogAscending =
+                from dog in dogs
+                orderby dog
+                select dog;
+
+            Console.WriteLine("\n\ndogAscending: ");
+            foreach (string dog in dogAscending)
+            {
+                Console.WriteLine(dog);
+            }
+
+            IEnumerable<string> dogsDescending =
+                from dog in dogs
+                orderby dog descending
+                select dog;
+
+            Console.WriteLine("\n\nDogsDescending: ");
+            foreach (string dog in dogsDescending)
+            {
+                Console.WriteLine(dog);
+            }
+
+            Main();
+        }
+
+        public static void sortAgeArray()
+        {
+            int[] ages = { 7, 12, 15, 35, 52 };
+
+            IEnumerable<int> agesAscending =
+                from age in ages
+                orderby age
+                select age;
+
+            Console.WriteLine("\n\nagesAscending: ");
+            foreach(int age in agesAscending)
+            {
+                Console.WriteLine(age);
+            }
+
+            IEnumerable<int> agesDescending =
+                from age in ages
+                orderby age descending
+                select age;
+
+            Console.WriteLine("agesDescending: ");
+            foreach(int age in agesDescending)
+            {
+                Console.WriteLine(age);
+            }
+
+            Main();
         }
     }
 }
